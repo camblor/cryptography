df459ac6a8cb52ac2dd0df1a4285166dbe265cff
camblor <alfonsocamblor@gmail.com>
2020-10-19 18:21:38 +0200

    this is the remix of parameters

--- afin.c
+++ afin.c
@@ -3,6 +3,8 @@
 #include <stdlib.h>
 #include <unistd.h>
 
+#define ERR -1
+
 
 /* Boolean data type*/
 typedef enum { false, true } bool;
@@ -21,72 +23,87 @@ typedef struct {
 /* MAIN FUNCTION */
 int main(int argc, char *argv[]) {
 
+    /* Parameter options*/
     parameters_afin options;
+
+    /* Required parameters */
+    bool ciphertext_size_flag = false;
+    bool afin_mult = false;
+    bool afin_const = false;
+
+    /* Error controller*/
     bool doDefault = false;
+
+    /* PROGRAM OPTIONS SELECTION */
+
+    /* Variable declaration */
     int c;
     opterr = 0;
 
+    /* Argument parsing with common arguments and list of possible options */
     while ((c = getopt (argc, argv, "CDm:a:b:i::o::")) != -1)
     {
+        /* Checks if the selected option is in our list*/
         switch (c)
         {
+            /* ENCRYPTION */
             case 'C':
                 options.mode = false;
                 break;
+
+            /* DECRYPTION */
             case 'D':
                 options.mode = true;
                 break;
+
+            /* CIPHERTEXT SPACE SITE */
             case 'm':
-            printf("m is %s\n", optarg);
-            fflush(stdout);
                 if (isdigit(*optarg) == 0){
                     doDefault = true;
                     break;
                 }
-                printf("m is %s\n", optarg);
-                fflush(stdout);
                 options.ciphertext_size = atoi(optarg);
-                printf("m value is %d\n", options.ciphertext_size);
+                ciphertext_size_flag = true;
                 break;
+            
+            /* MULTIPLICATIVE COEFFICIENT (AFIN) */
             case 'a':
                 if (!isdigit(*optarg)){
                         doDefault = true;
                         break;
                 }
                 options.afin_mult = atoi(optarg);
-                printf("a value is %d\n", options.afin_mult);
+                afin_mult = true;
                 break;
+
+            /* CONSTANT TERM (AFIN) */
             case 'b':
                 if (!isdigit(*optarg)){
                         doDefault = true;
                         break;
                 }
                 options.afin_const = atoi(optarg);
-                printf("b value is %d\n", options.afin_const);
+                afin_const = true;
                 break;
+            
+            /* INPUT FILE */
             case 'i':
-                if (!isdigit(*optarg)){
-                        doDefault = true;
-                        break;
-                }
-                options.input = atoi(optarg);
-                printf("i value is %d\n", options.input);
+                options.input = stdin;
                 break;
+            
+            /* OUTPUT FILE */
             case 'o':
-                if (!isdigit(optarg)){
-                        doDefault = true;
-                        break;
-                }
-                options.output = atoi(optarg);
-                printf("o value is %d\n", options.output);
+                options.output = stdout;
                 break;
+            
+            /* ERROR CONTROL */
             case '?':
                 doDefault = true;
-                break;
             default:
-                printf("hola has llegado.\n");
+                break;
         }
 
+        /* ERROR CONTROL FUNCTION FOR ARGUMENT PARSING */
         if (doDefault){
             if (optopt == 'm')
                 fprintf (stderr, "Option -%c requires an argument.\n", optopt);
@@ -105,4 +122,16 @@ int main(int argc, char *argv[]) {
                 return 1;
         }
     }
+    /* ERROR CONTROL, REQUIRED ARGUMENTS: e/d, m, a, b */
+        if( !ciphertext_size_flag || !afin_mult || !afin_const ){
+            fprintf (stderr, "REQUIRED ARGUMENTS:\n");
+            fprintf (stderr, "\t-m <tamaño del espacio de texto cifrado (value)>\n");
+            fprintf (stderr, "\t-a <coeficiente multiplicativo de la función afín (value)>\n");
+            fprintf (stderr, "\t-b <término constante de la función afín (value)>\n");
+        }
+
+        /* COMPROBAR ENTRE ENCRYPT Y DECRYPT MEDIANTE LA BANDERA options.mode */
+        /* TODO */
+
+        /* UTILIZAR LOS VALORES */
 }
