12057e2d961d566d338672ba0fd60060dae2392c
camblor <alfonsocamblor@gmail.com>
2020-10-29 01:20:16 +0100

    makefile

--- src/euclid.h
+++ src/euclid.h
@@ -1,4 +1,9 @@
+#ifndef eucld_crypt
+#define eucld_crypt
+
 int gcd(int a, int b);
 int gcdExtended(int a, int b, int *x, int *y);
 int euclid(int a, int b);
 int euclidExtended(int a, int b, int* x, int* y);
+
+#endif
